9b15db9ba8d1b954d3e09e65f917e1884a8f5209
Felipe Nogueira <86392886+NOG01@users.noreply.github.com>
2024-06-23 02:32:02 -0300

    Update pong-game.py

--- pong-game.py
+++ pong-game.py
@@ -8,6 +8,10 @@ def start_game():
     draw_game_screen()
 
 def draw_game_screen():
+    global left_player, right_player
+    left_player = 0
+    right_player = 0
+
     sc = turtle.Screen()
     sc.title("Pong Game")
     sc.bgcolor("black")
@@ -17,7 +21,7 @@ def draw_game_screen():
     left_pad.speed(0)
     left_pad.shape("square")
     left_pad.color("white")
-    left_pad.shapesize(stretch_wid=6,stretch_len=2)
+    left_pad.shapesize(stretch_wid=6, stretch_len=2)
     left_pad.penup()
     left_pad.goto(-400, 0)
 
@@ -25,7 +29,7 @@ def draw_game_screen():
     right_pad.speed(0)
     right_pad.shape("square")
     right_pad.color("white")
-    right_pad.shapesize(stretch_wid=6,stretch_len=2)
+    right_pad.shapesize(stretch_wid=6, stretch_len=2)
     right_pad.penup()
     right_pad.goto(400, 0)
 
@@ -35,11 +39,8 @@ def draw_game_screen():
     hit_ball.color("red")
     hit_ball.penup()
     hit_ball.goto(0, 0)
-    hit_ball.dx=7
-    hit_ball.dy=-7
-
-    left_player = 0
-    right_player = 0
+    hit_ball.dx = 7
+    hit_ball.dy = -7
 
     sketch = turtle.Turtle()
     sketch.speed(0)
@@ -47,9 +48,7 @@ def draw_game_screen():
     sketch.penup()
     sketch.hideturtle()
     sketch.goto(0, 260)
-    sketch.write("Player One : 0 Player Two: 0",
-                 align="center", font=("Courier",
-                                       24, "normal"))
+    update_score(sketch)
 
     def paddleaup():
         y = left_pad.ycor()
@@ -76,6 +75,7 @@ def draw_game_screen():
 
     while True:
         sc.update()
+
         hit_ball.setx(hit_ball.xcor() + hit_ball.dx)
         hit_ball.sety(hit_ball.ycor() + hit_ball.dy)
 
@@ -89,34 +89,25 @@ def draw_game_screen():
         if hit_ball.xcor() > 500:
             hit_ball.goto(0, 0)
             hit_ball.dy *= -1
-
-        sketch.clear()
-        sketch.write("Player One : {} Player Two : {}".format(
-            left_player, right_player), align="center",
-            font=("Courier", 24, "normal"))
-
+            left_player += 1
+            update_score(sketch)
         if hit_ball.xcor() < -500:
             hit_ball.goto(0, 0)
             hit_ball.dy *= -1
             right_player += 1
-            sketch.clear()
-            sketch.write("Player One : {} Player Two: {}".format(
-            left_player, right_player), align="center",
-            font=("Courier", 24, "normal"))
+            update_score(sketch)
 
-        if (hit_ball.xcor() > 360 and
-                            hit_ball.xcor() < 370) and (hit_ball.ycor()
-                                                        <right_pad.ycor()+40 and
-                                                        hit_ball.ycor() > right_pad.ycor()-40):
-                            hit_ball.setx(360)
-                            hit_ball.dx*=-1
+        if (360 > hit_ball.xcor() > 350) and (right_pad.ycor() + 50 > hit_ball.ycor() > right_pad.ycor() - 50):
+            hit_ball.setx(350)
+            hit_ball.dx *= -1
+        if (-360 < hit_ball.xcor() < -350) and (left_pad.ycor() + 50 > hit_ball.ycor() > left_pad.ycor() - 50):
+            hit_ball.setx(-350)
+            hit_ball.dx *= -1
 
-        if (hit_ball.xcor() < -360 and
-                            hit_ball.xcor()>-370) and (hit_ball.ycor()
-                                                       <left_pad.ycor()+40 and
-                                                       hit_ball.ycor() > left_pad.ycor()-40):
-                            hit_ball.setx(-360)
-                            hit_ball.dx*=-1
+def update_score(sketch):
+    sketch.clear()
+    sketch.write("Player One : {} Player Two : {}".format(left_player, right_player),
+                 align="center", font=("Courier", 24, "normal"))
 
 sc = turtle.Screen()
 sc.title("Pong Game Menu")
@@ -129,13 +120,14 @@ start_button.shape("square")
 start_button.color("white")
 start_button.penup()
 start_button.goto(0, 0)
-start_button.shapesize(stretch_wid=3, stretch_len=6)
+start_button.shapesize(stretch_wid=3, stretch_len=3)
 
-start_button.write("Start Game", align="center", font=("Courier", 24, "normal"))
+start_button.write("Start Game", align="center", font=("Courier", 70, "normal"))
 
 game_started = False
 
 def check_click(x, y):
+    global game_started
     if -60 < x < 60 and -30 < y < 30 and not game_started:
         start_game()
 
